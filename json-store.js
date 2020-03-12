1f2f3080ef0f0bfd42c67ab60f8181d338208551
douglasdtc <douglas@deltacon.com.br>
2020-03-12 08:47:29 -0300

    Update json-store.js

--- json-store.js
+++ json-store.js
@@ -2,43 +2,32 @@ var fs = require('fs');
 
 function Store(path) {
   this.path = path;
-  if (!fs.existsSync(path)) 
-    fs.writeFileSync(path, JSON.stringify({}));
+  if (!fs.existsSync(path)) fs.writeFileSync(path, JSON.stringify({}));
   this.Store = require(path);
 }
 
 Store.prototype.get = function(key) {
   if (!key) return clone(this.Store);
-  if (!this.Store[key]) return;
   return clone(this.Store[key]);
 }
 
 Store.prototype.set = function(key, value) {
   this.Store[key] = clone(value);
-}
-
-Store.prototype.setSync = function(key, value) {
-  this.Store[key] = clone(value);
-  this.saveSync();
+  this.save();
 }
 
 Store.prototype.del = function(key) {
   delete this.Store[key];
-}
-
-Store.prototype.delSync = function(key) {
-  delete this.Store[key];
-  this.saveSync();
+  this.save();
 }
 
 Store.prototype.save = function() {
-  fs.writeFile(this.path, JSON.stringify(this.Store));
-}
-Store.prototype.saveSync = function() {
   fs.writeFileSync(this.path, JSON.stringify(this.Store));
 }
 
 function clone(data) {
+  // JSON.parse(undefined) throws an error, so handle it explicitly
+  if (data === undefined) return undefined;
   return JSON.parse(JSON.stringify(data));
 }
 
